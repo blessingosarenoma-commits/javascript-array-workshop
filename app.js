7a2a340cce554058ab842735aca21b68366ff394
Osarenoma Blessing Dick <blessingosarenoma@gmail.com>
2025-09-09 14:41:26 +0100

    workshop

---
+++ app.js
@@ -0,0 +1,13 @@
+console.log("Good afternoon");
+const blogpost = [
+  "My first blogpost",
+  "My second blogpost",
+  "My third blogpost",
+  "My fourth blodpost",
+];
+console.log(blogpost);
+console.log(blogpost[1]);
+
+//i want to do an array of my favorite fruit in the array below//
+const favouritefruit = ["orange", "peach", "plums", "grape"];
+console.log(favouritefruit[3]);
